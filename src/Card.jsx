41ef89695e2458f4711da37e299993b0846d2052
Ashik150 <sskhan4409@gmail.com>
2024-11-17 00:16:33 +0600

    card done

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,8 +1,10 @@
+import Ashik from './assets/Ashik.jpg';
+
 function Card(){
     return(
-        <div className="card">
-            <img src="" alt="Ashik" />
-            <h1>Ashik</h1>
+        <div className="Card">
+            <img className='card-image' src={Ashik} alt="Ashik" />
+            <h1 className='card-title'>Ashik</h1>
             <p>I am Ashik and I am a student</p>
             </div>
     );
