755eef1b848481374f59695419fe96b50c729b48
Ashik150 <sskhan4409@gmail.com>
2024-11-16 23:39:48 +0600

    card half done

---
+++ src/Card.jsx
@@ -0,0 +1,11 @@
+function Card(){
+    return(
+        <div className="card">
+            <img src="" alt="Ashik" />
+            <h1>Ashik</h1>
+            <p>I am Ashik and I am a student</p>
+            </div>
+    );
+}
+
+export default Card;
