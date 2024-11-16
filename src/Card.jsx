372fe83db83a5c8ee02d65d183c27ff67ad256d6
Ashik150 <sskhan4409@gmail.com>
2024-11-17 00:17:49 +0600

    card error fixed

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,7 +2,7 @@ import Ashik from './assets/Ashik.jpg';
 
 function Card(){
     return(
-        <div className="Card">
+        <div className="card">
             <img className='card-image' src={Ashik} alt="Ashik" />
             <h1 className='card-title'>Ashik</h1>
             <p>I am Ashik and I am a student</p>
