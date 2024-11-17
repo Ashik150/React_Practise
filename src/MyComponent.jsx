d404baa3a92f76b45e85094ae5f844371a124b6c
Ashik150 <sskhan4409@gmail.com>
2024-11-17 13:11:08 +0600

    UseEffect

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,13 +1,29 @@
-import React,{useState} from 'react';
+import React, { useState, useEffect } from 'react';
 
 function MyComponent() {
-    const [name,setName] = useState("Don");
-  return(
-    <div>
-      <p>Name: {name}</p>
-      <button onClick={()=>setName("John")}>Set Name</button>
-    </div>
-  );
+    //const [name,setName] = useState("Don");
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        document.title = `Count: ${count}`;
+    }, [count]
+    );
+
+    function addCount() {
+        setCount(c => c + 1);
+    }
+
+    function subCount() {
+        setCount(c => c - 1);
+    }
+
+    return (
+        <>
+            <h1>Count: {count}</h1>
+            <button onClick={addCount}>Add</button>
+            <button onClick={subCount}>Subtract</button>
+        </>
+    );
 }
 
 export default MyComponent;
